b0201a187d2a8e2c067dc26de221fda09a09b6b5
ilanasufrin <ilanasufrin@gmail.com>
2017-08-04 20:23:17 -0400

    Clean up comments

--- graphs/implementations.py
+++ graphs/implementations.py
@@ -1,9 +1,11 @@
 """
-The following is an implementation of a directed, weighted, labeled graph with both a 2D-matrix and adjency list to
+The following is an implementation of a directed, weighted, labeled graph with BOTH a 2D-matrix and adjency list to
 keep track of vertices, edges, and weights.
 
 It's for illustrative purposes only- obviously it's redundant to store the same information in
 BOTH data structures.
+
+If there is no edge between any two nodes, the matrix is filled in with -1 as the default
 """
 
 class Vertex:
@@ -12,7 +14,6 @@ class Vertex:
         self.adjencyList = {}
 
     def addNeighbor(self,nbr,weight=0):
-        #insert into adjency list
         self.adjencyList[nbr] = weight
 
     def getConnections(self):
@@ -29,13 +30,13 @@ class Graph:
     def __init__(self):
         self.vertList = {}
         self.numVertices = 0
-        # self.matrix = [[-1]] #my attempt to ALWAYS have to resize it, but at least it was dynamic
-        self.matrix = [[-1 for x in range(10)] for y in range(10)] # TODO make it resizeable
+        # self.matrix = [[-1]] # that was my attempt to ALWAYS have to resize it, but at least it was dynamic
+        self.matrix = [[-1 for x in range(7)] for y in range(7)] # TODO make it resizeable
 
     def addVertex(self,key):
         # this is mostly relevant to the adjacency list
         if key in self.vertList:
-            raise Exception("vertex already exists")
+            raise Exception("Trying to add a vertex that already exists")
 
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
@@ -59,39 +60,42 @@ class Graph:
 
         return newVertex
 
-    def addEdge(self,f,t,cost=0):
+    def addEdge(self,f,t,weight=0):
         #insert into adjency list
         if f not in self.vertList:
             nv = self.addVertex(f)
         if t not in self.vertList:
             nv = self.addVertex(t)
-        self.vertList[f].addNeighbor(self.vertList[t], cost)
+        self.vertList[f].addNeighbor(self.vertList[t], weight)
 
         #insert into matrix
         if self.matrix[f][t] != -1:
-            print("Vertex at [%d][%d] was previously set to %d" %(f, t, self.matrix[f][t]))
+            print("Warning: Vertex at [%d][%d] was previously set to %d" %(f, t, self.matrix[f][t]))
 
-        self.matrix[f][t] = cost
+        self.matrix[f][t] = weight
 
 
     def getVerticesAList(self):
         return self.vertList.keys()
 
     def printMatrix(self):
-        print("called printMatrix")
         for i in range((len(self.matrix))):
             for j in range((len(self.matrix[0]))):
                 print("Row " + str(i) + " Column " + str(j) + " Edge weight " + (str(self.matrix[i][j])))
+
+    def printAdjacencyList(self):
+        for v in g.vertList.values():
+            for w in v.getConnections():
+                print("( %s , %s )" % (v.getId(), w.getId()))
 
 g = Graph()
 
 for i in range(6):
     g.addVertex(i)
 
-g.addVertex(5)
 
-# g.printMatrix()
 g.addEdge(0,1,5)
 g.addEdge(5,4,8)
 g.addEdge(5,4,6)
 g.printMatrix()
+g.printAdjacencyList()
